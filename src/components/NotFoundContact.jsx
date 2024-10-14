f4b683d3565651f8698b7c787a1fd058c5ac82bc
Shivam singh <shivamsingh.int@delhivery.com>
2024-10-14 20:29:11 +0530

    image path

--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -2,11 +2,10 @@ const NotFoundContact = () => {
   return (
     <div className="flex h-[80vh] items-center justify-center gap-4">
       <div>
-        <img src="/contact.png" />
+        <img src="/firebase-contact-app/contact.png" />
       </div>
       <h3 className="text-2xl font-semibold text-white"> Contact Not Found</h3>
     </div>
   );
 };
-
 export default NotFoundContact;
