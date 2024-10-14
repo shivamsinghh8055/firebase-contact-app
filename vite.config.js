cb735f67abad0a268cb462e58f7019c39ae7a642
Shivam singh <shivamsingh.int@delhivery.com>
2024-10-14 20:01:59 +0530

    issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/firebase-contact-app/', // Add this line
+  base: '/firebase-contact-app/' // Add this line
 });
 
